65a96184b005b3750b329d8a8ad6727fd5630da8
Francesc Elies Henar <francesc.elies@gmail.com>
2022-06-20 19:05:15 +0200

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-azure-devops',
-    version='0.1.1',
+    version='0.2.0',
     author='Francesc Elies',
     license='Mozilla Public License 2.0',
     url='https://github.com/FrancescElies/pytest-azure-devops',
